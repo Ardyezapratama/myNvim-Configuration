08960e5dd23afc00cc3a3f1571dc42c00b3ae8f1
Ardyezapratama <ardyezapratama@gmail.com>
2025-01-23 20:13:44 +0800

    Change colorscheme to nord and make the background transparent

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -15,7 +15,7 @@ require("lazy").setup({
   { import = "plugins" },
 } --[[@as LazySpec]], {
   -- Configure any other `lazy.nvim` configuration options here
-  install = { colorscheme = { "rose-pine", "astrotheme", "habamax" } },
+  install = { colorscheme = { "nord", "rose-pine", "astrotheme", "habamax" } },
   ui = { backdrop = 100 },
   performance = {
     rtp = {
